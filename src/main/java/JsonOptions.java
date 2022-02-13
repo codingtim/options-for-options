e0d4969f1634447e385abbf12d450dad46d7fa45
Tim Boeckstaens <boeckstaenstim@gmail.com>
2022-02-13 09:42:18 +0100

    Hypothetical 3 add copyright

--- src/main/java/JsonOptions.java
+++ src/main/java/JsonOptions.java
@@ -1,32 +1,40 @@
-public record JsonOptions(boolean indent, boolean indentArrays) {
+public record JsonOptions(boolean indent, boolean indentArrays, String copyright) {
 
     public JsonOptions(boolean indent) {
         //constructor for backwards compatibility
-        this(indent, false);
+        this(indent, false, null);
     }
 
     public static JsonOptions defaultOptions() {
         return new JsonOptions(
                 false,
-                false
+                false,
+                null
         );
     }
 
     public JsonOptions withIndentation() {
-        return new JsonOptions(true, indentArrays);
+        return new JsonOptions(true, indentArrays, copyright);
     }
 
     public JsonOptions withoutIndentation() {
-        return new JsonOptions(false, indentArrays);
+        return new JsonOptions(false, indentArrays, copyright);
     }
 
-
     public JsonOptions withArrayIndentation() {
-        return new JsonOptions(indent, true);
+        return new JsonOptions(indent, true, copyright);
     }
 
     public JsonOptions withoutArrayIndentation() {
-        return new JsonOptions(indent, false);
+        return new JsonOptions(indent, false, copyright);
+    }
+
+    public JsonOptions withCopyright(String copyright) {
+        return new JsonOptions(indent, indentArrays, copyright);
+    }
+
+    public JsonOptions withoutCopyright() {
+        return new JsonOptions(indent, indentArrays, null);
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
                 jsonOptions
                         .withIndentation()
                         .withoutArrayIndentation()
+                        .withCopyright("(c) 2022")
         );
     }
 }
