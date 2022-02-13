1118f9c5d76b322c5ab69d6997d29f316326a08e
Tim Boeckstaens <boeckstaenstim@gmail.com>
2022-02-13 09:38:08 +0100

    Hypothetical 2 add array indentation support

--- src/main/java/JsonOptions.java
+++ src/main/java/JsonOptions.java
@@ -1,17 +1,32 @@
-public record JsonOptions(boolean indent) {
+public record JsonOptions(boolean indent, boolean indentArrays) {
+
+    public JsonOptions(boolean indent) {
+        //constructor for backwards compatibility
+        this(indent, false);
+    }
 
     public static JsonOptions defaultOptions() {
         return new JsonOptions(
+                false,
                 false
         );
     }
 
     public JsonOptions withIndentation() {
-        return new JsonOptions(true);
+        return new JsonOptions(true, indentArrays);
     }
 
     public JsonOptions withoutIndentation() {
-        return new JsonOptions(false);
+        return new JsonOptions(false, indentArrays);
+    }
+
+
+    public JsonOptions withArrayIndentation() {
+        return new JsonOptions(indent, true);
+    }
+
+    public JsonOptions withoutArrayIndentation() {
+        return new JsonOptions(indent, false);
     }
 
 }
